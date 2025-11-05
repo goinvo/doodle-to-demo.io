0e78e42d6c1e590257894361de2af74d34b445b6
Jennifer Patel <jennifer@goinvo.com>
2025-11-05 11:57:34 -0500

    added case study 1, and also broke slides into separate files for modularity

---
+++ src/app/slides/slides/index.ts
@@ -0,0 +1,20 @@
+import Slide0 from './Slide0';
+import Slide1 from './Slide1';
+import Slide2 from './Slide2';
+import Slide3 from './Slide3';
+import Slide4 from './Slide4';
+import Slide5 from './Slide5';
+import Slide6 from './Slide6';
+
+// Export slides as an array of components
+// Slide0 accepts nextIndex prop, others don't
+export const slides = [
+  Slide0,
+  Slide1,
+  Slide2,
+  Slide3,
+  Slide4,
+  Slide5,
+  Slide6,
+];
+
